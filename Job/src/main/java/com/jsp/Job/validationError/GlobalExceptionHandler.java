964b18a2aea5a612eb5ac45712edfddd4de91a46
DEEPSIKASRI <deepsiganesh@gmail.com>
2024-03-26 11:48:53 +0530

    Adding validation class

---
+++ Job/src/main/java/com/jsp/Job/validationError/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.jsp.Job.validationError;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List <String> errors = new ArrayList <> ();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.add(fieldName + ": " + errorMessage);
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
